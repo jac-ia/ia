8e43b87ea7839a5e1818f4bec8615019c7777c27
jac-ia <itemalert1@gmail.com>
2024-06-30 10:51:21 -0600

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,6 +4,9 @@ pkg install openssh
 sshd
 passwd usuario
 termux-setup-storage
+mkdir .termux/boot
+curl -o .termux/boot/start-ssh.sh https://pastebin.com/raw/Wdu63dZs
+curl -o .termux/boot/start-ia.sh https://pastebin.com/raw/vPun3Hun
 pkg install python-pip -y
 pip install selenium==4.9.1
 pkg install x11-repo -y
