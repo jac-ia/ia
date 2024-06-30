d1e508300cd3d3d6950c465a91af39e62ab22e1b
jac-ia <itemalert1@gmail.com>
2024-06-30 10:57:56 -0600

    Update and rename install.sh to install1.sh

---
+++ install1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pkg update -y
+pkg install openssh
+sshd
+passwd usuario
+termux-setup-storage
+mkdir .termux/boot
+curl -o .termux/boot/start-ssh.sh https://pastebin.com/raw/Wdu63dZs
+curl -o .termux/boot/start-ia.sh https://pastebin.com/raw/vPun3Hun
+pkg install python-pip -y
+pip install selenium==4.9.1
+pkg install x11-repo -y
+pkg install tur-repo -y
+pkg install chromium -y
+curl -o config.ini https://pastebin.com/raw/Pm8jhdrB
+curl -o actualizar.py https://pastebin.com/raw/ANsknUy1
+echo "Proceso finalizado"
