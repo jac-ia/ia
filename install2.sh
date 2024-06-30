2d0b1ac9108cd9fb445923f1db1211ff7cb1155b
jac-ia <itemalert1@gmail.com>
2024-06-30 10:42:00 -0600

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pkg update -y
+pkg install openssh
+sshd
+passwd usuario
+termux-setup-storage
+pkg install python-pip -y
+pip install selenium==4.9.1
+pkg install x11-repo -y
+pkg install tur-repo -y
+pkg install chromium -y
+curl -o actualizar.py https://pastebin.com/raw/NPPSaQge
+echo "Proceso finalizado"
