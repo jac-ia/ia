43daf40bec796d4ace15f0881f8089be71ae6353
jac-ia <itemalert1@gmail.com>
2024-06-30 11:02:11 -0600

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -6,7 +6,9 @@ passwd usuario
 termux-setup-storage
 mkdir .termux/boot
 curl -o .termux/boot/start-ssh.sh https://pastebin.com/raw/Wdu63dZs
+chmod +x .termux/boot/start-ssh.sh
 curl -o .termux/boot/start-ia.sh https://pastebin.com/raw/vPun3Hun
+chmod +x .termux/boot/start-ia.sh
 pkg install python-pip -y
 pip install selenium==4.9.1
 pkg install x11-repo -y
